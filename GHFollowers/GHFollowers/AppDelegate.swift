4dc1c47e924c293f8759319eb08d614d761e10b8
TaherLamloum <dr.taherpharma@gmail.com>
2024-10-22 22:48:08 +0300

    initial project

---
+++ GHFollowers/GHFollowers/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  GHFollowers
+//
+//  Created by taher elnehr on 16/10/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+     
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+         searchVC.title = "Search"
+         searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    func createFavouriteNC() -> UINavigationController {
+        let favouriteListVC = FavouriteListVC()
+        favouriteListVC.title = "Favourites"
+        favouriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favouriteListVC)
+    }
+    func createTabBar() -> UITabBarController {
+        let tabBar = UITabBarController()
+        UITabBar().tintColor = .systemGreen
+        
+        tabBar.viewControllers = [createSearchNC(), createFavouriteNC()]
+        return tabBar
+    }
+}
+
---
+++ GHFollowers/GHFollowers/FavouriteListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteListVC.swift
+//  GHFollowers
+//
+//  Created by taher elnehr on 17/10/2024.
+//
+
+import UIKit
+
+class FavouriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
